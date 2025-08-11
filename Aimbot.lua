814d9168db3462ef84c726e4d24968afd59c09f2
Raof1212 <raofkan5@gmail.com>
2025-08-11 05:53:02 +0100

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -10,23 +10,23 @@ local rootPart = character:WaitForChild("HumanoidRootPart")
 local flying = false
 local flySpeed = 50
 
-local bodyVelocity = Instance.new("BodyVelocity")
-bodyVelocity.MaxForce = Vector3.new(1e5,1e5,1e5)
-bodyVelocity.Velocity = Vector3.new(0,0,0)
-bodyVelocity.P = 1250
-bodyVelocity.Parent = rootPart
-bodyVelocity.Enabled = false
+local bodyPosition = Instance.new("BodyPosition")
+bodyPosition.MaxForce = Vector3.new(1e5, 1e5, 1e5)
+bodyPosition.P = 10000
+bodyPosition.D = 1000
+bodyPosition.Parent = rootPart
+bodyPosition.Position = rootPart.Position
+bodyPosition.Enabled = false
 
 local function toggleFly()
 	flying = not flying
-	bodyVelocity.Enabled = flying
-	humanoid.PlatformStand = flying  -- prevent physics issues when flying
+	bodyPosition.Enabled = flying
+	humanoid.PlatformStand = flying
+
 	if flying then
 		print("Fly mode activated")
 	else
 		print("Fly mode deactivated")
-		bodyVelocity.Velocity = Vector3.new(0,0,0)
-		humanoid.PlatformStand = false
 	end
 end
 
@@ -59,11 +59,6 @@ RunService.Heartbeat:Connect(function()
 			moveDirection = moveDirection + right
 		end
 
-		moveDirection = moveDirection.Unit
-		if moveDirection ~= moveDirection then -- NaN check
-			moveDirection = Vector3.new(0,0,0)
-		end
-
 		if UserInputService:IsKeyDown(Enum.KeyCode.Space) then
 			moveDirection = moveDirection + Vector3.new(0,1,0)
 		end
@@ -71,9 +66,14 @@ RunService.Heartbeat:Connect(function()
 			moveDirection = moveDirection + Vector3.new(0,-1,0)
 		end
 
-		bodyVelocity.Velocity = moveDirection * flySpeed
+		if moveDirection.Magnitude > 0 then
+			moveDirection = moveDirection.Unit
+			bodyPosition.Position = rootPart.Position + moveDirection * flySpeed
+		else
+			bodyPosition.Position = rootPart.Position
+		end
 	else
-		bodyVelocity.Velocity = Vector3.new(0,0,0)
+		bodyPosition.Position = rootPart.Position
 	end
 end)
 
@@ -83,8 +83,8 @@ LocalPlayer.CharacterAdded:Connect(function(char)
 	character = char
 	humanoid = character:WaitForChild("Humanoid")
 	rootPart = character:WaitForChild("HumanoidRootPart")
-	bodyVelocity.Parent = rootPart
-	bodyVelocity.Enabled = false
+	bodyPosition.Parent = rootPart
+	bodyPosition.Enabled = false
 	humanoid.PlatformStand = false
 	flying = false
 end)
