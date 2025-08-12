5c552ae73fcc1d5fc8e1c81fc55796d176a446cd
Raof1212 <raofkan5@gmail.com>
2025-08-12 19:13:19 +0100

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -10,11 +10,11 @@ local Camera = Workspace.CurrentCamera
 
 -- List of teammate usernames to ignore (up to 10)
 local TeammatesUsernames = {
-    "hamza_x007j",
-    "Roben121200",
-    "ALG_DZ3",
-    "mikey7y77",
-    "haithem123k",
+    "",
+    "",
+    "",
+    "",
+    "",
     "Player6",
     "Player7",
     "Player8",
@@ -126,6 +126,7 @@ RunService.RenderStepped:Connect(function()
         currentTarget = nil
     end
 end)
+
 
 
 
